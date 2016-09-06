712abb9214b076dc525ce90fabb819144fb86536
Lars Hjerrild <201409555@post.au.dk>
2016-09-06 20:44:47 +0200

    A self updating solution

---
+++ Lifeway/Training.Test.Unit/RunningUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Training.Test.Unit
+{
+    [TestFixture]
+    public class RunningUnitTests
+    {
+        [Test]
+        public void Add_Add2And4_Expect6()
+        {
+            Assert.That(6, Is.EqualTo(Running.Add(2, 4)));
+        }
+
+    }
+}
---
+++ Lifeway/Training/Running.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    public class Running
+    {
+        public static double Add(double a1,double a2 )
+        {
+            return a1+a2;
+        }
+    }
+}
